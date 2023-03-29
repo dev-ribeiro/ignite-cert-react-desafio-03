97152ff509d550f7756b632a67d0331e2e778f76
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-03-29 16:12:19 -0300

    fix: adjust update cart function

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -98,9 +98,30 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      if (amount <= 0) {
+        return;
+      }
+
+      const stock = await api.get(`/stock/${productId}`);
+      const stockAmount = stock.data.amount;
+
+      if (amount > stockAmount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+
+      const updatedCart = [...cart];
+      const productAlreadyExists = updatedCart.find(product => product.id === productId);
+
+      if (productAlreadyExists) {
+        productAlreadyExists.amount = amount;
+        setCart(updatedCart);
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(updatedCart));
+      } else {
+        throw Error()
+      }
     } catch {
-      // TODO
+      toast.error('Erro na alteração de quantidade do produto');
     }
   };
 
